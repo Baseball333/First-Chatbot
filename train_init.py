301b685aaf8aabcfe8c5a7b2d244718f17a6f491
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-17 14:42:22 -0800

    Add tokenized words

--- train_init.py
+++ train_init.py
@@ -4,7 +4,7 @@ output_empty = [0] * len(classes)
 for doc in documents:
      # Initialize bag of words
      bag = []
-  
-  
+     # List of tokenized words for the pattern
+     pattern_words =  doc[0]
   
   
