008b097c72ac9437f499ecd740ae77721d4984ea
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-17 15:02:40 -0800

    Specify output

--- train_init.py
+++ train_init.py
@@ -11,4 +11,11 @@ for doc in documents:
      # Create the bag of words array with 1 if word is matched in pattern
      for w in words:
           bag.append(1) if w in pattern_words else bag.append(0)
-                      
+          
+      # Output is a "0" for each tag and "1" for the current tag
+      output_row = list(output_empty)
+      output_row[classes.index(doc[1])] = 1
+     
+          
+          
+          
