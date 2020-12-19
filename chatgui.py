608c111151158d16f64483a42e445059569d9b4b
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-19 09:06:14 -0800

    Initiate the message

--- chatgui.py
+++ chatgui.py
@@ -1,3 +1,7 @@
 # Create GUI with tkinter library
 import tkinter
 from tkinter import *
+
+      def send():
+             msg = EntryBox.get("1.0", "end-1c").strip()
+             EntryBox.delete("0.0", END)
