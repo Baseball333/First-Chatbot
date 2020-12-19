861875ee3831ed1289209aa2b1023309280cbb60
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-18 16:27:12 -0800

    Add each class to the class list

--- words.py
+++ words.py
@@ -6,4 +6,9 @@ for intent in intents["intents"]:
                words.extend(w)
                # Add each document
                documents.append((w, intent["tag"]))
-  
+               
+               # Add each class to the class list
+               if intent["tag"] not in classes:
+                  classes.append(intent["tag"])
+                                    
+               
