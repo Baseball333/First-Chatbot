6efdbe88e54d149bbaf0c589c50f6278e5edfe6c
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-19 09:10:30 -0800

    Update all non-string text

--- chatgui.py
+++ chatgui.py
@@ -5,3 +5,9 @@ from tkinter import *
       def send():
              msg = EntryBox.get("1.0", "end-1c").strip()
              EntryBox.delete("0.0", END)
+
+             if msg != "":
+                   ChatLog.config(state=NORMAL)
+                   Chatlog.insert(END, "You: " + msg + "\n\n")
+                   ChatLog.config(foreground="#442265", font=(("Verdana", 12))
+                                  
