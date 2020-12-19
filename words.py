3f4dd11453d055d3d43a1721ca8b9b1a2996e6c7
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-18 16:14:48 -0800

    Tokenize each word

--- words.py
+++ words.py
@@ -1,4 +1,7 @@
 for intent in intents["intents"]:
    for pattern in intents["patterns"]
     
-
+               # Tokenize each word
+               w = nltk_word.tokenize(pattern)
+               words.extend(w)
+     
