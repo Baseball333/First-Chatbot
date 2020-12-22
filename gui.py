91bada88b9567f8845cc339732d7bb53fc15d8d9
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-21 20:06:05 -0800

    Write intents, words and classes

--- gui.py
+++ gui.py
@@ -2,9 +2,9 @@ from keras.models import load_model
 model = load_model("chatbot_model.h5")
 import json
 import random
-
-
-
+intents = json.loads(open("intents.json").read())
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
 
 
 
