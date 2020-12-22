0735b7ad72b47aee566ad3a2d01e527288d3e4da
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-12-21 20:07:14 -0800

    Define the clean_sentences function

--- gui.py
+++ gui.py
@@ -6,8 +6,8 @@ intents = json.loads(open("intents.json").read())
 words = pickle.load(open("words.pkl", "rb"))
 classes = pickle.load(open("classes.pkl", "rb"))
 
-
-
+# Extract information from the files
+def clean_sentences(sentences):
 
 
 
